1cbef26d4a47b113e807b2c47a35b2dce97af701
Pranjal Mishra <mishrapranjal232@gmail.com>
2025-05-05 23:44:21 +0530

    fourgt

--- nova.py
+++ nova.py
@@ -101,7 +101,9 @@ def nova_command():
 
 # ========== MAIN ==========
 if __name__ == "__main__":
-    print("Nova Flask backend running at http://127.0.0.1:5000")
+    print("Nova Flask backend running...")
     speak("Ram Ram Ram Boss!")
     speak("Hey, Nova is ready to assist you boss!")
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
